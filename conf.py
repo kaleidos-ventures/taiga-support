41b1fd8f8b569d570fae3acee536f4fd85efb81c
David Barragán Merino <david.barragan@kaleidos.net>
2015-10-19 20:12:52 +0200

    Enable TOC markdoen plugin

--- conf.py
+++ conf.py
@@ -919,7 +919,7 @@ PRETTY_URLS = True
 # Note: most Nikola-specific extensions are done via the Nikola plugin system,
 #       with the MarkdownExtension class and should not be added here.
 # The default is ['fenced_code', 'codehilite']
-MARKDOWN_EXTENSIONS = ['fenced_code', 'codehilite', 'extra']
+MARKDOWN_EXTENSIONS = ['toc', 'fenced_code', 'codehilite', 'extra']
 
 # Extra options to pass to the pandoc comand.
 # by default, it's empty, is a list of strings, for example
